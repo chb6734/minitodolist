c01a28b370e425673467294ee0540d3ec5445e91
chb6734 <chb6734@gmail.com>
2023-02-07 16:06:01 +0900

    feat: navbar 추가

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import "./App.css";
 import React from "react";
+import Todolist from "./components/Todolist";
+import AddTodo from "./components/AddTodo";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <div className="wrapper">
-      <div className="content"></div>
+      <div className="content">
+        <NavBar />
+        <Todolist />
+        <AddTodo />
+      </div>
     </div>
   );
 }
